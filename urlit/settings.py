1c7f13c41dfa9101f92495d0817434bf491a2c9e
Thejus-M <81758774+Thejus-M@users.noreply.github.com>
2022-10-01 16:04:48 +0530

    Update settings.py

--- urlit/settings.py
+++ urlit/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY=SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','thejus-m.is-a.dev']
 
 
 
